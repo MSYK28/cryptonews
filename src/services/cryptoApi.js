f7834de56e3cc45fe92e1cedd5d3eff0752637ba
MSYK <jimmule8@gmail.com>
2022-01-26 17:46:02 +0300

    initial commit for cryptonews

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'b824ac850emsha92a1c3170a8d67p198031jsn2cde3a27e655'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
